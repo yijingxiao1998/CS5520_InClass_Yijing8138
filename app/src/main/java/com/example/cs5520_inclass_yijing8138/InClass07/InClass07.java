939ea8f479bea6c4ae2de1da70b158edd06d0047
SherryYijing <48372728+SherryYijing@users.noreply.github.com>
2023-03-20 15:17:13 -0400

    Committing InClass07.

--- app/src/main/java/com/example/cs5520_inclass_yijing8138/InClass07/InClass07.java
+++ app/src/main/java/com/example/cs5520_inclass_yijing8138/InClass07/InClass07.java
@@ -8,20 +8,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cs5520_inclass_yijing8138.R;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -29,8 +21,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 import okhttp3.Call;
